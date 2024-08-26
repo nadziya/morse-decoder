a7852cfd7cea0f42b96f7fd39f94a118c695ef2b
Nadziya Frolova <sunny1976@mail.ru>
2024-08-26 19:33:26 +0400

    feat: develop solution for Morse Decoder task

--- src/index.js
+++ src/index.js
@@ -36,9 +36,49 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 function decode(expr) {
-    // write your solution here
+    let letter = [];
+    let letterMorse = '';
+    let realLetter = '';
+    let result = '';
+    let realLetterMassiv = [];
+    for (let i = 0; i < expr.length; i = i + 10){
+          let a = expr.slice(i, i + 10);
+         letter.push(a);
+    }
+    for (let j = 0; j < letter.length; j++) {
+        for (let k = 0; k < 10; k = k + 2) {
+         let findNum = `${letter[j][k]}${letter[j][k + 1]}`;
+            if (findNum === '00'){
+               letterMorse = `${letterMorse}`;
+            } else if (findNum === '10'){
+             letterMorse = `${letterMorse}.`;
+            } else if (findNum === '11'){
+            letterMorse = `${letterMorse}-`;
+            } else if (letter[j] === '**********'){
+            letterMorse = `${letterMorse}`;
+            }
+        }
+        realLetterMassiv.push(letterMorse);
+        letterMorse = '';
+        
+     }
+        
+    for (let m = 0; m < realLetterMassiv.length; m++){
+        for (let key in MORSE_TABLE) {
+            if (realLetterMassiv[m] === key){
+                realLetter = MORSE_TABLE[key];
+                result = `${result}${realLetter}`;
+            }
+            
+        }
+        if (realLetterMassiv[m] === '') {
+                let space = ' ';
+                result = `${result}${space}`;
+            }
+    }  
+    return result;// write your solution here
 }
 
 module.exports = {
